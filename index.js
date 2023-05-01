81e8e5656594891039dd8e7ed90bd04f322d87c7
paulniloy <110883440+paulniloy@users.noreply.github.com>
2023-05-02 00:04:55 +0600

    chef details added done

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const cors = require('cors');
+
+const chefdetails = require("./chef.json")
+
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('server World!')
 })
-app.get('/helllo', (req, res) => {
-  res.send('server dear hello!')
+app.get('/chef', (req, res) => {
+  res.send(chefdetails)
 })
 
 app.listen(port, () => {
