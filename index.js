3ef8eaf78f22887c89546df8c89921c3ef4460b7
paulniloy <110883440+paulniloy@users.noreply.github.com>
2023-05-03 03:44:01 +0600

    json file updated

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const port = 3000
 const cors = require('cors');
 
 const chefdetails = require("./chef.json")
+const specificres = require("./recipies.json")
 
 app.use(cors());
 
@@ -17,6 +18,14 @@ app.get('/chef/:id', (req, res) => {
     const id = req.params.id;
     const specific = chefdetails.find(details => details.id === id)
   res.send(specific)
+})
+app.get('/recipies', (req, res) => {
+  res.send(specificres)
+})
+app.get('/recipies/:id', (req, res) => {
+  const id = req.params.id;
+  const found = specificres.filter(chef=>chef.category_id == id);
+  res.send(found)
 })
 
 app.listen(port, () => {
